26ff6b20e7aa1c6412c38b2d91c7d3199a272305
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-14 14:07:11 +0100

    Vector2 and wall push

--- M03UF4_BREAKOUT_3/Vector2.h
+++ M03UF4_BREAKOUT_3/Vector2.h
@@ -1,1 +1,11 @@
 #pragma once
+
+class Vector2 {
+public:
+	int x;
+	int y;
+	Vector2();
+	Vector2(int x, int y);
+
+};
+
--- M03UF4_BREAKOUT_3/Wall.h
+++ M03UF4_BREAKOUT_3/Wall.h
@@ -1,1 +1,13 @@
 #pragma once
+enum WallType { HORIZONTAL, VERTICAL, CORNER };
+
+
+class Wall {
+
+private:
+	 Vector2 position
+
+
+
+
+};
