da9c5683aceb23be824316cb41cbd410d446ccbf
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-14 14:16:58 +0100

    Pad

--- M03UF4_BREAKOUT_3/Pad.h
+++ M03UF4_BREAKOUT_3/Pad.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "Wall.h"
+#include "Vector2.h"
+
+class Pad {
+private:
+	Vector2 position;
+	int width;
+public:
+
+	Pad(Vector2 p, int w);
+	int GetWidth();
+	void SetWidth(int w);
+	void Update(Wall[] walls);
+	void SetPosition(Vector2 pos);
+	void Render();
+};
