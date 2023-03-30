1ea0ed220c60d7b4d311c2170e7fbab29391e20c
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-30 13:01:30 +0200

    Menu

--- M03UF4_BREAKOUT_3/GameManager.cpp
+++ M03UF4_BREAKOUT_3/GameManager.cpp
@@ -18,9 +18,29 @@ void GameManager::Update() {
 }
 
 void GameManager::Menu() {
-	int sleepTime = 16;
-	std::cout << "Menu" << std::endl;
-	Sleep(sleepTime);
+	int sleepTime = 500;
+	bool keyPressed = false;
+	bool press1;
+	bool press2;
+	while (!keyPressed) {
+		std::cout << "Main Menu \n\n";
+		std::cout << "Press 1 to play" << std::endl;
+		std::cout << "Press 2 to exit" << std::endl;
+		
+		 press1 = GetAsyncKeyState('1') != 0;
+		 press2 = GetAsyncKeyState('2') != 0;
+		
+		 keyPressed = press1 || press2;
+
+		Sleep(sleepTime);
+		system("cls");
+
+	}
+
+	if (press1)
+		currentScene = Scene::GAMEPLAY;
+	else 
+		isPlaying = false;
 }
 
 void GameManager::GamePlay() {
