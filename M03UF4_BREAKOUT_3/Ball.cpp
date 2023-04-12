b3487796cdbea0a3e22bebd13985b5bcb92334fb
AleixTRP <116338424+AleixTRP@users.noreply.github.com>
2023-04-12 17:58:01 +0200

    Pala rebotes

--- M03UF4_BREAKOUT_3/Ball.cpp
+++ M03UF4_BREAKOUT_3/Ball.cpp
@@ -1,35 +1,40 @@
 #include "Ball.h"
 
-void Ball::Update(std::vector<Wall> walls, std::vector<Brick>& bricks, Pad* pads) {
+void Ball::Update(std::vector<Wall> walls, std::vector<Brick>& bricks, Pad* pad) {
+    Vector2 targetPos = position + direction;
 
-	Vector2 targetPos = position + direction;
+    for (auto it = walls.begin(); it != walls.end(); it++) {
+        if (it->GetPosition() == targetPos) {
+            // colisión con pared
+            switch (it->GetType()) {
+            case VERTICAL:
+                direction.x *= -1;
+                break;
+            case HORIZONTAL:
+                direction.y *= -1;
+                break;
+            case CORNER:
+                direction.x *= -1;
+                direction.y *= -1;
+                break;
+            }
+        }
+    }
 
-	for (auto it = walls.begin(); it != walls.end(); it++) {
-		if (it->GetPosition() == targetPos) {
-			switch (it->GetType())
-			{
-			case VERTICAL:
-				direction.x *= -1;
-				break;
-			case HORIZONTAL:
-				direction.y *= -1;
+    for (auto it = bricks.begin(); it != bricks.end(); it++) {
+        if (it->GetPosition() == targetPos && it->GetHealth() != 0) {
+            // colisión con ladrillo
+            it->TakeDamage(GetDamage());
+            direction.y *= -1;
+        }
+    }
 
-				break;
-			case CORNER:
-				direction.x *= -1;
-				direction.y *= -1;
-				break;
-			}
-		}
-	}
-	for (auto it = bricks.begin(); it != bricks.end(); it++) {
-		if (it->GetPosition() == targetPos && it->GetHealth() != 0) {
-			it->TakeDamage(GetDamage());
+    // verificar colisión con la paleta
+    Vector2 padPos = pad->GetPosition();
+    int padWidth = pad->GetWidth();
+    if (targetPos.y == padPos.y - 1 && targetPos.x >= padPos.x && targetPos.x <= padPos.x + padWidth - 1) {
+        direction.y *= -1;
+    }
 
-			direction.y *= -1;
-
-		}
-	}
-	position = position + direction;
-
+    position = position + direction;
 }
